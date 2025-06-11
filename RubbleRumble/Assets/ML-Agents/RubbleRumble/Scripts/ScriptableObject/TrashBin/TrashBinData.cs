88876b46f5f88f0ad5bca4d7b0c4213c68f13740
kangdawoo <qpfmtldks12@naver.com>
2025-06-11 12:42:01 +0900

    test
    test

--- RubbleRumble/Assets/ML-Agents/RubbleRumble/Scripts/ScriptableObject/TrashBin/TrashBinData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TrashBin", menuName = "TrashBins/TrashBinData")]
-public class TrashBinData : ScriptableObject
-{
-    public string ableTrash;
-}
